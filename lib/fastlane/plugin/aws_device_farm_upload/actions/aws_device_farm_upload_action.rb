7b67bcdd9042178fa957cf41156ab0eb381d9892
Takuma Homma <nagomimatcha@gmail.com>
2021-01-09 22:57:36 +0900

    Prepare upload action

--- lib/fastlane/plugin/aws_device_farm_upload/actions/aws_device_farm_upload_action.rb
+++ lib/fastlane/plugin/aws_device_farm_upload/actions/aws_device_farm_upload_action.rb
@@ -5,7 +5,58 @@ module Fastlane
   module Actions
     class AwsDeviceFarmUploadAction < Action
       def self.run(params)
-        UI.message("The aws_device_farm_upload plugin is working!")
+
+        file_path = params[:file_path]
+        file_name = params[:file_name]
+        file_type = params[:file_type]
+
+        access_key_id = params[:aws_access_key_id]
+        secret_access_key = params[:aws_secret_access_key]
+
+        project_arn = params[:device_farm_project_arn]
+        
+        require 'aws-device-farm'
+        device_farm_client = Aws::DeviceFarm::Client.new(
+          region: 'us-west-2',
+          credentials: Aws::Credentials.new(access_key_id, secret_access_key)
+        )
+
+        # Some files cannot update, so delete first when uploads file with same name.
+        delete_file(device_farm_client, project_arn, file_name, file_type)
+        upload_file(device_farm_client, project_arn, file_name, file_path, file_type)
+      end
+
+      def self.delete_file(device_farm_client, project_arn, file_name, file_type)
+        uploaded_list = device_farm_client.list_uploads({
+          arn: project_arn,
+          type: file_type
+        })
+        uploaded_file = uploaded_list.uploads.find do |upload|
+          upload.name == file_name && upload.status == 'SUCCEEDED'
+        end
+
+        unless uploaded_file.nil?
+          devicefarm_client.delete_upload({
+            arn: uploaded_file.arn
+          })
+        end
+      end
+
+      def self.upload_file(device_farm_client, project_arn, file_name, file_path, file_type)
+        params = {
+          project_arn: project_arn,
+          name: file_name,
+          type: file_type,
+          content_type: 'application/octet-stream'
+        }
+
+        create_request_response = device_farm_client.create_upload(params)
+        url = URI.parse(create_request_response.upload.url)
+        target = File.open(file_path, 'rb').read
+
+        response = Net::HTTP.start(url.host) do |http|
+          http.send_request('PUT', url.request_uri, target, {'content-type' => 'application/octet-stream'})
+        end
       end
 
       def self.description
